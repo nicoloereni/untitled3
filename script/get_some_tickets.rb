0302ea4220f63b1dd9b8df8c4c8f1c110a16b6c2
Nicolo Ereni <nicolo.ereni@nozominetworks.com>
2019-11-09 13:03:51 +0100

    script point to expected cookie

--- script/get_some_tickets.rb
+++ script/get_some_tickets.rb
@@ -3,5 +3,6 @@ require_relative '../lib/cookie.rb'
 @url = 'https://tickets.events.ccc.de/36c3/'
 
 # Cookie.new(@url).ask_until_get(4, 'intro_seen_36c3') { p 'DONE!' }
+
 Cookie.new(@url).ask_until_get(4, '36C3Queue') { p 'DONE!' }
 # Cookie.new(@url).ask_until_get(4, 'pretix_csrftoken') { p 'DONE!' }
